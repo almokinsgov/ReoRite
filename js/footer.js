499304791f750e16ff9ec8722fc61fbb16b5ec76
almokinsgov <olafbig@hotmail.com>
2025-04-30 00:15:51 +1200

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -9,8 +9,8 @@ document.addEventListener("DOMContentLoaded", function () {
       <div class="footer-section">
         <h4>Learn More</h4>
         <ul>
-          <li><a href="https://github.com/almokinsgov/ReoRite/wiki" target="_blank">📄 Wiki</a></li>
-          <li><a href="https://reorite.nz/compare" target="_blank">🔊 Comparison Page</a></li>
+          <li><a href="https://github.com/almokinsgov/ReoRite/wiki" target="_self">📄 Wiki</a></li>
+          <li><a href="https://reorite.nz/compare" target="_self">🔊 Comparison Page</a></li>
           <li><a href="https://drive.google.com/drive/folders/1eWqhweSHGdlaSAIIxqr0xQ9kK-32VJzN" target="_blank">📄 Research Paper</a></li>
           <li><a href="https://github.com/almokinsgov/ReoRite" target="_blank">💻 GitHub Repo</a></li>
         </ul>
@@ -27,11 +27,11 @@ document.addEventListener("DOMContentLoaded", function () {
         <h4>Contact</h4>
         <ul>
           <li><a href="mailto:amorangim@hotmail.com">📬 Email Amorangi</a></li>
-          <li><a href="https://reorite.nz" target="_blank">🌐 ReoRite.nz</a></li>
+          <li><a href="https://reorite.nz" target="_self">🌐 ReoRite.nz</a></li>
         </ul>
       </div>
     </div>
-    <div class="footer-note">© 2024 Amorangi Mathews – ReoRite.nz</div>
+    <div class="footer-note">© 2025 Amorangi Mathews – ReoRite.nz</div>
   `;
   document.body.appendChild(footer);
 });
