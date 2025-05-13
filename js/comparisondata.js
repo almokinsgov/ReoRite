67e1404ae03c64ee27ea83522fc9410b4cb82b66
almokinsgov <olafbig@hotmail.com>
2025-05-13 23:56:17 +1200

    Update comparisondata.js

--- js/comparisondata.js
+++ js/comparisondata.js
@@ -149,7 +149,7 @@ const comparisonData = [
         ipa: "/ N/A /",
         without: "audio/tena_without.mp3",
         with: "audio/tena_with.mp3",
-        notes: "Word combination.<br><b>Accuracy rating before correction:</b> -%<br><b>Accuracy rating after correction:</b> -%"
+        notes: "This phrase-level example previews Reo Rite’s future direction: sentence segmentation and pronunciation support, grounded in natural Māori prosody.<br><b>Accuracy rating before correction:</b> -%<br><b>Accuracy rating after correction:</b> -%"
       }
     ];
 
